6a92a878914655383cefdeb02efc58a1bf3428aa
APIWE <corneliaxozwa@gmail.com>
2020-02-18 23:23:19 +0200

    Calculating sum

---
+++ src/main/java/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args){
+        int number1 = 5;
+        int number2 = 10;
+        int answer = 0;
+
+        answer = number1 + number2;
+        System.out.println("The sum is: " + answer);
+    }
+}

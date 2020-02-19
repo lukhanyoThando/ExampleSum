a20aa059dfd74bf933d829ce98cc96ac068296c7
LukhanyoThando <thandonko141@gmail.com>
2020-02-19 08:24:50 +0200

    I have added a subtraction code

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -2,9 +2,16 @@ public class Sum {
     public static void main(String[] args){
         int number1 = 5;
         int number2 = 10;
-        int answer = 0;
+        int answer;
+
+        int num1 = 6, num2 = 3, ans;
+
 
         answer = number1 + number2;
         System.out.println("The sum of two numbers is: " + answer);
+
+        // subtraction
+        ans = num1- num2;
+        System.out.println("The answer of the subtraction is "+ " "+ ans);
     }
 }
